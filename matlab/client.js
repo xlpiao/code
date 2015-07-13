478a3d15df779fb2adbaa74323a5808b72877ce8
Hyanglan Park <hlpark@company100.com>
2015-07-13 15:14:19 +0900

    create new dir and backup matlab and python dir

---
+++ matlab/client.js
@@ -0,0 +1,30 @@
+net = require('net');
+
+function Client(host, port){
+    this.queue = [];
+    this.socket = new net.Socket();
+    this.socket.connect(port, host, function() {
+        console.log('Connected');
+    });
+}
+
+Client.prototype.send = function (data){
+    this.socket.write(data+'\n');
+}
+
+Client.prototype.receive = function (){
+    var that = this;
+    this.socket.on('data', function(data) {
+        that.queue.push(data);
+        console.log(''+data);
+    });
+}
+
+Client.prototype.disconnect = function (){
+    this.socket.on('close', function() {
+        console.log('Connection closed');
+        this.socket.destroy();
+    });
+}
+
+module.exports = Client;
