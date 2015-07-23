ba9fdf2547be98f25923b8e52de5504fe4003247
xlpiao <xianglan0502@gmail.com>
2015-07-24 01:21:11 +0900

    delete files and check in new files

--- matlab/socket_node_matlab/server.m
+++
@@ -1,7 +0,0 @@
-function [Q socket] = server(host, port)
-    Q = Queue();
-    socket = tcpip(host,port,'NetworkRole','Server');
-    set(socket, 'InputBufferSize', 3000000);
-    set(socket, 'OutputBufferSize', 3000000);
-    fopen(socket);
-end
