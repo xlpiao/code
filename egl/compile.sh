78a63ce17dc791fbee6c7a29d858f4262ee91735
Xianglan Piao <xianglan0502@gmail.com>
2016-10-11 21:10:43 +0900

    export library path if not work

--- egl/compile.sh
+++ egl/compile.sh
@@ -1,3 +1,6 @@
+# export LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:/system/lib
+# export C_INCLUDE_PATH=/usr/local/include:$C_INCLUDE_PATH
+
 cc -g `pkg-config --cflags gbm` `pkg-config --cflags egl` `pkg-config --cflags gl` -std=c99 -o egl_gbm egl_gbm.c `pkg-config --libs gbm`  `pkg-config --libs egl` `pkg-config --libs gl`
 
 cc -g `pkg-config --cflags gbm` `pkg-config --cflags egl` `pkg-config --cflags gl` -std=c99 -o egl_example egl_example.c `pkg-config --libs gbm`  `pkg-config --libs egl` `pkg-config --libs gl`
