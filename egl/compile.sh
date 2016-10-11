d98405a58a7e892cb72dae3aeb501c512c56b5ef
Xianglan Piao <xianglan0502@gmail.com>
2016-10-11 21:08:59 +0900

    add compile code for all C files

--- egl/compile.sh
+++ egl/compile.sh
@@ -1,1 +1,3 @@
-cc -g `pkg-config --cflags gbm` `pkg-config --cflags egl` `pkg-config --cflags gl` -std=c99 -o output egl_gbm.c `pkg-config --libs gbm`  `pkg-config --libs egl` `pkg-config --libs gl`
+cc -g `pkg-config --cflags gbm` `pkg-config --cflags egl` `pkg-config --cflags gl` -std=c99 -o egl_gbm egl_gbm.c `pkg-config --libs gbm`  `pkg-config --libs egl` `pkg-config --libs gl`
+
+cc -g `pkg-config --cflags gbm` `pkg-config --cflags egl` `pkg-config --cflags gl` -std=c99 -o egl_example egl_example.c `pkg-config --libs gbm`  `pkg-config --libs egl` `pkg-config --libs gl`
