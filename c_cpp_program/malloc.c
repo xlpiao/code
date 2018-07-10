754ff95f38f866f9f69e82a618b219ef92b8610e
Xianglan Piao <xianglan0502@gmail.com>
2018-07-10 16:07:58 +0900

    Add matrix, malloc example

---
+++ c_cpp_program/malloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void)
+{
+    char *str;
+
+   /* malloc() allocate the memory for 5 integers */
+   /* containing garbage values */
+   str = (char *) malloc(15 * sizeof(char)); // 5*4bytes = 5 bytes
+
+   /* Deallocates memory previously allocated by malloc() function */
+   free( str );
+
+   /* calloc() allocate the memory for 5 integers and */
+   /* set 0 to all of them */
+   str = (char *) calloc(15, sizeof(char));
+
+    strcpy(str, "tutorialspoint");
+    printf("String = %s\n", str);
+
+    /* Reallocating memory */
+    str = (char *) realloc(str, 30);
+    strcat(str, ".com");
+    printf("String = %s\n", str);
+
+    free(str);
+
+    return 0;
+}
+
---
+++ c_cpp_program/matrix.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>  // rand(), srand()
+#include <time.h>    // time()
+
+#define M 4 
+#define N 3
+
+#define MIN_NUM 5 
+#define MAX_NUM 10
+
+void genMat (int **mat, int row, int col)
+{
+    /* Generate random integer between MIN_NUM and MAX_NUM */
+    srand(time(NULL));
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            /* NOTE: random int between 0 and 19 */
+            // int r = rand() % 20;
+            mat[i][j] = rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM;
+            // *(*(mat + i) + j) = rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM;
+        }
+    }
+}
+
+void printMat (int **mat, int row, int col) 
+{
+    printf("mat[%d][%d] = {\n", row, col);
+    for (int i = 0; i < row; i++) {
+        printf("{");
+        for (int j = 0; j < col; j++) {
+            if (j < col - 1)
+                // printf("%2d, ", *(*(mat + i) + j));
+                printf("%2d, ", mat[i][j]);
+            else
+                // printf("%2d", *(*(mat + i) + j));
+                printf("%2d", mat[i][j]);
+        }
+        if (i < row - 1)
+            printf("},\n");
+        else
+            printf("} ");
+    }
+    printf("}\n\n");
+}
+
+
+int main (void)
+{
+    int **mat1 = (int **) malloc(M * sizeof(int *));
+    for (int i = 0; i < M; i++)
+         mat1[i] = (int *)malloc(N * sizeof(int));
+
+    int **mat2 = (int **) malloc(M * sizeof(int *));
+    for (int j = 0; j < N; j++)
+         mat2[j] = (int *)malloc(M * sizeof(int));
+
+    /* Generate 2D matrix */
+    genMat(mat1, M, N);
+    genMat(mat2, N, M);
+
+    /* Print the generated random number */
+    printMat(mat1, M, N);
+    printMat(mat2, N, M);
+
+    for (int i = 0; i < M; i++)
+        free(mat1[i]);
+    free(mat1);
+
+    for (int j = 0; j < N; j++)
+        free(mat2[j]);
+    free(mat2);
+
+    return 0;
+}
