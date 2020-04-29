955a57e132980bced41e7643225ee1db0fc8c09f
Xianglan Piao <xianglan0502@gmail.com>
2020-04-30 02:40:30 +0900

    [feat] setup git hooks - add pre-commit: autoformat source code - add prepare-commit-msg: commit message format guide

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File              : setup.sh
+# Author            : Xianglan Piao <xianglan0502@gmail.com>
+# Date              : 2013.03.21
+# Last Modified Date: 2020.04.30
+# Last Modified By  : Xianglan Piao <xianglan0502@gmail.com>
+
+git config --local core.hooksPath $PWD/githooks
