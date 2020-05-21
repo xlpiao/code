acde75030197e4a5059b10803db7ea89b5fb0697
Xianglan Piao <xianglan0502@gmail.com>
2020-05-21 22:00:59 +0900

    [feat] minor update - minor update

--- gpu/cuda/src/masterclass/dim.cu
+++ gpu/cuda/src/masterclass/dim.cu
@@ -6,10 +6,12 @@ __global__ void print1D(int* input) {
 
   int gid = x_axis;
 
-  input[gid] = gid;
+  // input[gid] = gid;
 
-  printf("gridDim(%d), blockDim(%d), blockIdx(%d), threadIdx(%d), gid: %d\n",
-         gridDim.x, blockDim.x, blockIdx.x, threadIdx.x, gid);
+  printf(
+      "gridDim(%d), blockDim(%d), blockIdx(%d), threadIdx(%d), input(%d), "
+      "gid(%d)\n",
+      gridDim.x, blockDim.x, blockIdx.x, threadIdx.x, input[gid], gid);
 }
 
 __global__ void print2D(int* input) {
@@ -19,13 +21,13 @@ __global__ void print2D(int* input) {
 
   int gid = y_axis * gridDim.x * blockDim.x + x_axis;
 
-  input[gid] = gid;
+  // input[gid] = gid;
 
   printf(
       "gridDim(%d,%d), blockDim(%d,%d), blockIdx(%d,%d), threadIdx(%d,%d), "
-      "gid: %d\n",
+      "input(%d), gid(%d)\n",
       gridDim.x, gridDim.y, blockDim.x, blockDim.y, blockIdx.x, blockIdx.y,
-      threadIdx.x, threadIdx.y, gid);
+      threadIdx.x, threadIdx.y, input[gid], gid);
 }
 
 __global__ void print3D(int* input) {
@@ -37,14 +39,20 @@ __global__ void print3D(int* input) {
   int gid = z_axis * blockDim.x * gridDim.x * blockDim.y * gridDim.y +
             y_axis * blockDim.x * gridDim.x + x_axis;
 
-  input[gid] = gid;
+  // input[gid] = gid;
 
   printf(
       "gridDim(%d,%d,%d), blockDim(%d,%d,%d), blockIdx(%d,%d,%d), "
-      "threadIdx(%d,%d,%d), gid: %d\n",
+      "threadIdx(%d,%d,%d), input(%d), gid(%d)\n",
       gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z,
       blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z,
-      gid);
+      input[gid], gid);
+}
+
+void initInput(int* input, int size) {
+  for (int index = 0; index < size; index++) {
+    input[index] = index;
+  }
 }
 
 int main(void) {
@@ -58,8 +66,9 @@ int main(void) {
   //// 1D
   printf("\nprint 1D:\n");
   h_input = (int*)calloc(size.x, sizeof(int));
+  initInput(h_input, size.x);
   cudaMalloc((void**)&d_input, size.x * sizeof(int));
-  // cudaMemcpy(d_input, h_input, size.x * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_input, h_input, size.x * sizeof(int), cudaMemcpyHostToDevice);
 
   block_dim.x = 2;
   grid_dim.x = size.x / block_dim.x;
@@ -67,14 +76,17 @@ int main(void) {
   print1D<<<grid_dim, block_dim>>>(d_input);
   cudaDeviceSynchronize();
 
-  // cudaMemcpy(h_input, d_input, size.x * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_input, d_input, size.x * sizeof(int), cudaMemcpyDeviceToHost);
   free(h_input);
   cudaFree(d_input);
 
   //// 2D
   printf("\nprint 2D:\n");
   h_input = (int*)calloc(size.x * size.y, sizeof(int));
+  initInput(h_input, size.x * size.y);
   cudaMalloc((void**)&d_input, size.x * size.y * sizeof(int));
+  cudaMemcpy(d_input, h_input, size.x * size.y * sizeof(int),
+             cudaMemcpyHostToDevice);
 
   block_dim.y = 4;
   grid_dim.y = size.y / block_dim.y;
@@ -82,15 +94,18 @@ int main(void) {
   print2D<<<grid_dim, block_dim>>>(d_input);
   cudaDeviceSynchronize();
 
-  // cudaMemcpy(h_input, d_input, size.x * size.y * sizeof(int),
-  // cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_input, d_input, size.x * size.y * sizeof(int),
+             cudaMemcpyDeviceToHost);
   free(h_input);
   cudaFree(d_input);
 
   //// 3D
   printf("\nprint 3D:\n");
   h_input = (int*)calloc(size.x * size.y * size.z, sizeof(int));
+  initInput(h_input, size.x * size.y * size.z);
   cudaMalloc((void**)&d_input, size.x * size.y * size.z * sizeof(int));
+  cudaMemcpy(d_input, h_input, size.x * size.y * size.z * sizeof(int),
+             cudaMemcpyHostToDevice);
 
   block_dim.z = 8;
   grid_dim.z = size.z / block_dim.z;
@@ -98,8 +113,8 @@ int main(void) {
   print3D<<<grid_dim, block_dim>>>(d_input);
   cudaDeviceSynchronize();
 
-  // cudaMemcpy(h_input, d_input, size.x * size.y * size.z * sizeof(int),
-  // cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_input, d_input, size.x * size.y * size.z * sizeof(int),
+             cudaMemcpyDeviceToHost);
   free(h_input);
   cudaFree(d_input);
 
