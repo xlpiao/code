28410e3e6ff26449e76451d9ab1b99b5ed20b25c
Xianglan Piao <xianglan0502@gmail.com>
2020-05-21 14:01:29 +0900

    [chore] minor update - minor

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # File              : setup.sh
 # Author            : Xianglan Piao <xianglan0502@gmail.com>
-# Date              : 2013.03.21
-# Last Modified Date: 2020.04.30
+# Date              : 2020.04.30
+# Last Modified Date: 2020.05.21
 # Last Modified By  : Xianglan Piao <xianglan0502@gmail.com>
 
 git config --local core.hooksPath $PWD/hooks
