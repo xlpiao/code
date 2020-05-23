fd2fbfc2a76d8f6c8d66839cae559f47fc30d887
Xianglan Piao <xianglan0502@gmail.com>
2020-05-23 23:44:50 +0900

    [feat] add dim_v1 - when global size cannot divide block size exactly (ex. with remainder exist) - griddim = size/blockdim + 1

--- gpu/cuda/src/masterclass/dim_v1.cu
+++ gpu/cuda/src/masterclass/dim_v1.cu
@@ -1,52 +1,58 @@
 #include <stdio.h>
 
-__global__ void print1D(int* input) {
+__global__ void print1D(int* input, dim3 size) {
   int axis_x = blockIdx.x * blockDim.x + threadIdx.x;
-  // printf("%d\n", axis_x);
+  // printf("%d, %d\n", axis_x, size.x);
 
   int gid = axis_x;
 
   // input[gid] = gid;
 
-  printf(
-      "gridDim(%d), blockDim(%d), blockIdx(%d), threadIdx(%d), input(%d), "
-      "gid(%d)\n",
-      gridDim.x, blockDim.x, blockIdx.x, threadIdx.x, input[gid], gid);
+  if (axis_x < size.x) {
+    printf(
+        "size(%d), blockDim(%d), blockIdx(%d), threadIdx(%d), input(%d), "
+        "gid(%d)\n",
+        size.x, blockDim.x, blockIdx.x, threadIdx.x, input[gid], gid);
+  }
 }
 
-__global__ void print2D(int* input) {
+__global__ void print2D(int* input, dim3 size) {
   int axis_x = blockIdx.x * blockDim.x + threadIdx.x;  // col
   int axis_y = blockIdx.y * blockDim.y + threadIdx.y;  // row
-  // printf("%d, %d\n", axis_x, axis_y);
+  // printf("%d, %d, %d, %d\n", axis_x, axis_y, size.x, size.y);
 
-  int gid = axis_y * gridDim.x * blockDim.x + axis_x;
+  int gid = axis_y * size.x + axis_x;
 
   // input[gid] = gid;
 
-  printf(
-      "gridDim(%d,%d), blockDim(%d,%d), blockIdx(%d,%d), threadIdx(%d,%d), "
-      "input(%d), gid(%d)\n",
-      gridDim.x, gridDim.y, blockDim.x, blockDim.y, blockIdx.x, blockIdx.y,
-      threadIdx.x, threadIdx.y, input[gid], gid);
+  if (axis_x < size.x && axis_y < size.y) {
+    printf(
+        "size(%d,%d), blockDim(%d,%d), blockIdx(%d,%d), threadIdx(%d,%d), "
+        "input(%d), gid(%d)\n",
+        size.x, size.y, blockDim.x, blockDim.y, blockIdx.x, blockIdx.y,
+        threadIdx.x, threadIdx.y, input[gid], gid);
+  }
 }
 
-__global__ void print3D(int* input) {
+__global__ void print3D(int* input, dim3 size) {
   int axis_x = blockIdx.x * blockDim.x + threadIdx.x;
   int axis_y = blockIdx.y * blockDim.y + threadIdx.y;
   int axis_z = blockIdx.z * blockDim.z + threadIdx.z;
-  // printf("%d, %d, %d\n", axis_x, axis_y, axis_z);
+  // printf("%d, %d, %d, %d, %d, %d\n", axis_x, axis_y, axis_z, size.x, size.y,
+  // size.z);
 
-  int gid = axis_z * blockDim.x * gridDim.x * blockDim.y * gridDim.y +
-            axis_y * blockDim.x * gridDim.x + axis_x;
+  int gid = axis_z * size.x * size.y + axis_y * size.x + axis_x;
 
   // input[gid] = gid;
 
-  printf(
-      "gridDim(%d,%d,%d), blockDim(%d,%d,%d), blockIdx(%d,%d,%d), "
-      "threadIdx(%d,%d,%d), input(%d), gid(%d)\n",
-      gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z,
-      blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z,
-      input[gid], gid);
+  if (axis_x < size.x && axis_y < size.y && axis_z < size.z) {
+    printf(
+        "size(%d,%d,%d), blockDim(%d,%d,%d), blockIdx(%d,%d,%d), "
+        "threadIdx(%d,%d,%d), input(%d), gid(%d)\n",
+        size.x, size.y, size.z, blockDim.x, blockDim.y, blockDim.z, blockIdx.x,
+        blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z,
+        input[gid], gid);
+  }
 }
 
 void initInput(int* input, int size) {
@@ -71,9 +77,9 @@ int main(void) {
   cudaMemcpy(d_input, h_input, size.x * sizeof(int), cudaMemcpyHostToDevice);
 
   block_dim.x = 2;
-  grid_dim.x = size.x / block_dim.x;
+  grid_dim.x = size.x / block_dim.x + 1;
 
-  print1D<<<grid_dim, block_dim>>>(d_input);
+  print1D<<<grid_dim, block_dim>>>(d_input, size);
   cudaDeviceSynchronize();
 
   cudaMemcpy(h_input, d_input, size.x * sizeof(int), cudaMemcpyDeviceToHost);
@@ -89,9 +95,9 @@ int main(void) {
              cudaMemcpyHostToDevice);
 
   block_dim.y = 4;
-  grid_dim.y = size.y / block_dim.y;
+  grid_dim.y = size.y / block_dim.y + 1;
 
-  print2D<<<grid_dim, block_dim>>>(d_input);
+  print2D<<<grid_dim, block_dim>>>(d_input, size);
   cudaDeviceSynchronize();
 
   cudaMemcpy(h_input, d_input, size.x * size.y * sizeof(int),
@@ -108,9 +114,9 @@ int main(void) {
              cudaMemcpyHostToDevice);
 
   block_dim.z = 8;
-  grid_dim.z = size.z / block_dim.z;
+  grid_dim.z = size.z / block_dim.z + 1;
 
-  print3D<<<grid_dim, block_dim>>>(d_input);
+  print3D<<<grid_dim, block_dim>>>(d_input, size);
   cudaDeviceSynchronize();
 
   cudaMemcpy(h_input, d_input, size.x * size.y * size.z * sizeof(int),
