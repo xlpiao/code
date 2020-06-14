6b9af9b6834c2dbeb727fd8a609f1b861d1bf5f7
Xianglan Piao <xianglan0502@gmail.com>
2020-06-14 23:35:03 +0900

    [feat] add cpu sequential 2d convolutional function - 2d convolution: conv2d

--- problem_solving/convolution/cpu_conv.cc
+++ problem_solving/convolution/cpu_conv.cc
@@ -2,11 +2,14 @@
 #include <vector>
 
 using Array1D = std::vector<float>;
+using Array2D = std::vector<Array1D>;
 
 class Functional {
  public:
   Array1D conv1d(Array1D& input, Array1D& filter, int stride = 1,
-                 int padding = 1);
+                 int padding = 0);
+  Array2D conv2d(Array2D& input, Array2D& filter, int stride = 1,
+                 int padding = 0);
 };
 
 Array1D Functional::conv1d(Array1D& input, Array1D& filter, int stride,
@@ -23,15 +26,51 @@ Array1D Functional::conv1d(Array1D& input, Array1D& filter, int stride,
   return output;
 }
 
-int main(void) {
-  Array1D input(5, 1);
-  Array1D filter(3, 2);
+Array2D Functional::conv2d(Array2D& input, Array2D& filter, int stride,
+                           int padding) {
+  const unsigned int row = input.size();
+  const unsigned int col = input[0].size();
+  Array2D output(row, Array1D(col, 0));
 
-  Functional F;
-  auto output = F.conv1d(input, filter);
-  for (auto it : output) {
-    std::cout << it << std::endl;
+  for (int i = 0; i < input.size(); i++) {
+    for (int j = 0; j < input[i].size(); j++) {
+      for (int m = 0; m < filter.size(); m++) {
+        for (int n = 0; n < filter.size(); n++) {
+          output[i][j] += input[i][j] * filter[m][n];
+        }
+      }
+    }
   }
+
+  return output;
+}
+
+int main(void) {
+  Functional F;
+
+  //// 1. 1D array convolution
+  Array1D input1(5, 1);
+  Array1D filter1(3, 2);
+
+  auto output1 = F.conv1d(input1, filter1);
+  for (auto it : output1) {
+    std::cout << it << ", ";
+  }
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  //// 2. 2D array convolution
+  Array2D input2(5, Array1D(5, 1));
+  Array2D filter2(3, Array1D(3, 2));
+
+  auto output2 = F.conv2d(input2, filter2);
+  for (int i = 0; i < output2.size(); i++) {
+    for (int j = 0; j < output2[i].size(); j++) {
+      std::cout << output2[i][j] << ", ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
 
   return 0;
 }
