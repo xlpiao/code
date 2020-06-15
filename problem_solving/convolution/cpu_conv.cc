2dfdc70c19a455831bb157659250ee1059daa18d
Xianglan Piao <xianglan0502@gmail.com>
2020-06-15 22:51:25 +0900

    [feat] conv2d - conv2d with c++ - conv2d with pytorch

--- problem_solving/convolution/cpu_conv.cc
+++ problem_solving/convolution/cpu_conv.cc
@@ -46,15 +46,24 @@ Array1D Functional::conv1d(Array1D& input, Array1D& filter,
 Array2D Functional::conv2d(Array2D& input, Array2D& filter,
                            const unsigned int stride,
                            const unsigned int padding) {
-  const unsigned int row = input.size();
-  const unsigned int col = input[0].size();
+  const unsigned int row =
+      (input.size() + 2 * padding - filter.size()) / stride + 1;
+  const unsigned int col =
+      (input[0].size() + 2 * padding - filter.size()) / stride + 1;
   Array2D output(row, Array1D(col, 0));
 
   for (int i = 0; i < output.size(); i++) {
     for (int j = 0; j < output[i].size(); j++) {
+      int row_offset = i * stride - padding;
+      int col_offset = j * stride - padding;
       for (int m = 0; m < filter.size(); m++) {
         for (int n = 0; n < filter.size(); n++) {
-          output[i][j] += input[i][j] * filter[m][n];
+          if ((row_offset + m) >= 0 && (col_offset + n) >= 0 &&
+              (row_offset + m) < (int)input.size() &&
+              (col_offset + n) < (int)input[i].size()) {
+            output[i][j] +=
+                input[m + row_offset][n + col_offset] * filter[m][n];
+          }
         }
       }
     }
@@ -98,10 +107,29 @@ int main(void) {
   std::cout << std::endl;
 
   //// 2. 2D array convolution
+  std::cout << "\n--- 2D convolution ---\n" << std::endl;
   Array2D input2(input_size, Array1D(input_size, 1));
   Array2D filter2(filter_size, Array1D(filter_size, 2));
 
-  auto output2 = F.conv2d(input2, filter2);
+  std::cout << "input: " << std::endl;
+  for (int i = 0; i < input2.size(); i++) {
+    for (int j = 0; j < input2[i].size(); j++) {
+      std::cout << input2[i][j] << ", ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  std::cout << "filter: " << std::endl;
+  for (int i = 0; i < filter2.size(); i++) {
+    for (int j = 0; j < filter2[i].size(); j++) {
+      std::cout << filter2[i][j] << ", ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  auto output2 = F.conv2d(input2, filter2, stride, padding);
   for (int i = 0; i < output2.size(); i++) {
     for (int j = 0; j < output2[i].size(); j++) {
       std::cout << output2[i][j] << ", ";
--- problem_solving/convolution/pytorch_conv.py
+++ problem_solving/convolution/pytorch_conv.py
@@ -1,54 +1,20 @@
 ## https://pytorch.org/docs/stable/nn.functional.html
 
-'''
-torch.nn.functional.conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1) → Tensor
-
-input: input tensor of shape (\text{minibatch} , \text{in\_channels} , iH , iW)(minibatch,in_channels,iH,iW)
-weight: filters of shape (\text{out\_channels} , \frac{\text{in\_channels}}{\text{groups}} , kH , kW)(out_channels,
-        groups
-        in_channels
-        ​
-         ,kH,kW)
-bias: optional bias tensor of shape (\text{out\_channels})(out_channels) . Default: None
-stride: the stride of the convolving kernel. Can be a single number or a tuple (sH, sW). Default: 1
-padding: implicit paddings on both sides of the input. Can be a single number or a tuple (padH, padW). Default: 0
-dilation: the spacing between kernel elements. Can be a single number or a tuple (dH, dW). Default: 1
-groups: split input into groups, \text{in\_channels}in_channels should be divisible by the number of groups. Default: 1
-'''
-
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
-input = torch.Tensor(np.array([[[
-    [1,1,1,0,0],
-    [0,1,1,1,0],
-    [0,0,1,1,1],
-    [0,0,1,1,0],
-    [0,1,1,0,0]
-]]]))
+input = torch.ones(1, 1, 1, 8)
+filter = torch.ones(1, 1, 1, 5) * 2
+out = F.conv1d(input, filter, stride = 2, padding = 2)
+print(input)
+print(filter)
+print(out)
 
-filter = torch.Tensor(np.array([[[
-    [1,0,1],
-    [0,1,0],
-    [1,0,1]
-]]]))
-
-input = Variable(input)
-filter = Variable(filter)
-out = F.conv2d(input, filter)
-# print(input)
-# print(filter)
-# print(out)
-
-
-'''
-torch.randn(*size, out=None, dtype=None, layout=torch.strided, device=None, requires_grad=False) → Tensor
-'''
-input = torch.randn(1,1,5,5)
-filter = torch.randn(1,1,3,3)
-out = F.conv2d(input, filter, padding=1)
+input = torch.ones(1, 1, 8, 8)
+filter = torch.ones(1, 1, 5, 5) * 2
+out = F.conv2d(input, filter, stride = 2, padding = 2)
 print(input)
 print(filter)
 print(out)
