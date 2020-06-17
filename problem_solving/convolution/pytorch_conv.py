dc9033d590f735cf38b746b41574a63137c0deb7
Xianglan Piao <xianglan0502@gmail.com>
2020-06-17 20:11:12 +0900

    [feat] 3d convolution - conv3d with pytorch and cpu sequential version with c/c++/cpp

--- problem_solving/convolution/pytorch_conv.py
+++ problem_solving/convolution/pytorch_conv.py
@@ -16,3 +16,12 @@ output = F.conv2d(input, kernel, stride = 2, padding = 2)
 print(input)
 print(kernel)
 print(output)
+
+input = torch.ones(1, 8, 8, 8)
+kernel = torch.ones(1, 5, 5, 5) * 2
+input = input.unsqueeze(0)
+kernel = kernel.unsqueeze(0)
+output = F.conv3d(input, kernel, stride = 2, padding = 2)
+print(input)
+print(kernel)
+print(output)
