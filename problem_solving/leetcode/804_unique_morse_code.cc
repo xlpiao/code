24f6c54909886a8ac464f3a3145bdcc08ad2426b
Xianglan Piao <xianglan0502@gmail.com>
2021-05-23 23:59:10 +0900

    [feat] leetcode 804 - 804 Unique Morse Code Words

---
+++ problem_solving/leetcode/804_unique_morse_code.cc
@@ -0,0 +1,43 @@
+/**
+ * File              : 1_two_sum.cc
+ * Author            : Xianglan Piao <xianglan0502@gmail.com>
+ * Date              : 2021.05.23
+ * Last Modified Date: 2021.05.23
+ * Last Modified By  : Xianglan Piao <xianglan0502@gmail.com>
+ */
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  int uniqueMorseRepresentations(std::vector<std::string>& words) {
+    std::vector<std::string> code{
+        ".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",
+        ".---", "-.-",  ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.",
+        "...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--.."};
+
+    std::unordered_map<std::string, int> morseRep;
+    for (int i = 0; i < words.size(); i++) {
+      std::string encode;
+      for (int j = 0; j < words[i].size(); j++) {
+        int idx = words[i][j] - 'a';
+        encode.append(code[idx]);
+      }
+      morseRep.emplace(encode, 0);
+    }
+
+    return morseRep.size();
+  }
+};
+
+int main(void) {
+  Solution s;
+  std::vector<std::string> words{"gin", "zen", "gig", "msg"};
+
+  auto result = s.uniqueMorseRepresentations(words);
+  std::cout << result << std::endl;
+
+  return 0;
+}
