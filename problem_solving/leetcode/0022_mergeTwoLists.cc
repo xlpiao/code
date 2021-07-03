0febcc0479efb00d03b8f2f848004271deaa5352
Xianglan Piao <xianglan0502@gmail.com>
2021-07-04 00:40:48 +0900

    Create 0022_mergeTwoLists.cc

---
+++ problem_solving/leetcode/0022_mergeTwoLists.cc
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* n1 = l1;
+    ListNode* n2 = l2;
+
+    while(n1 || n2) {
+      if(n1 && n2 && n1->val <= n2->val) {
+        n1 = n1->next;
+      }
+      else if(n1 && n2 && n1->val > n2->val) {
+        n1->val = n2->val;
+        n1->next = n1;
+        n2 = n2->next;
+        n1 = n1->next;
+      }
+      cout << n1->val << ", ";
+    }
+
+    return n1;
+  }
+};
