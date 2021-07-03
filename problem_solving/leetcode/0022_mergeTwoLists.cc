87bfbf1467899a1ec050e03603763daf1b6a6490
Xianglan Piao <xianglan0502@gmail.com>
2021-07-04 03:11:04 +0900

    Update 0022_mergeTwoLists.cc

--- problem_solving/leetcode/0022_mergeTwoLists.cc
+++ problem_solving/leetcode/0022_mergeTwoLists.cc
@@ -11,19 +11,38 @@
 class Solution {
 public:
   ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    ListNode* n1 = l1;
-    ListNode* n2 = l2;
-
-    while(n2) {
-      if(n1->val >=n2->val) {
-        n1->next = n1;
-        n1->val = n2->val;
-        n2 = n2->next;
+    if(!l1 && !l2) return NULL;
+    
+    ListNode* n = new ListNode();
+    ListNode *root = n;
+      
+    
+    while(l1 || l2) {
+      if(l1 && l2) {
+        if(l1->val <= l2->val) {
+          n->val = l1->val;
+          n->next = new ListNode();
+          l1 = l1->next;
+        }
+        else{
+          n->val = l2->val;
+          n->next = new ListNode();
+          l2 = l2->next;
+        }
       }
-      n1 = n1->next;
-      cout << n1->val << ", ";
+      else if (l1) {
+        n->val = l1->val;
+        n->next = l1->next;
+        break;
+      }
+      else if(l2) {
+        n->val = l2->val;
+        n->next = l2->next;
+        break;
+      }
+      cout << n->val << ", ";
+      n = n->next;
     }
-
-    return n1;
+    return root;
   }
 };
