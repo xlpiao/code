c26fd2ed0705f2b3468ab0556acac22b3765249a
Xianglan Piao <lanxlpiao@gmail.com>
2023-04-05 07:54:08 +0900

    [feat] update

--- setup.sh
+++ setup.sh
@@ -5,4 +5,17 @@
 # Last Modified Date: 2020.05.21
 # Last Modified By  : Xianglan Piao <lanxlpiao@gmail.com>
 
+file=.clang-format
+
+clang-format -style=google -dump-config > ${file}
+
+if [ "$(uname)" = "Linux" ]; then
+  sed -i '/^$/d' ${file}
+elif [ "$(uname)" = "Darwin" ]; then
+  sed -i '' -e '$d' ${file}
+else
+  sed -i '/^$/d' ${file}
+fi
+
+git add ${file}
 git config --local core.hooksPath $PWD/hooks
