0c060eb5db0e28f0f6efa43b2dae35e68bdb8703
Xianglan Piao <lanxlpiao@gmail.com>
2023-05-24 22:24:21 +0900

    [feat] update

--- cpp_tutorials/private_protected_public.cc
+++ cpp_tutorials/private_protected_public.cc
@@ -1,43 +1,63 @@
 #include <iostream>
 
+#define LOG() printf("%s +%d\t%s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__)
+
 class Base {
  private:
   int num1 = 1;
 
+ private:
+  void privateFunc() {
+    LOG();
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    std::cout << num3 << std::endl;
+  }
+
  protected:
   int num2 = 2;
 
+ protected:
+  void protectedFunc() {
+    LOG();
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    std::cout << num3 << std::endl;
+  }
+
  public:
   int num3 = 3;
-  void showData() {
-    std::cout << "Base Data" << std::endl;
+
+ public:
+  void publicFunc() {
+    LOG();
     std::cout << num1 << std::endl;
     std::cout << num2 << std::endl;
     std::cout << num3 << std::endl;
   }
 };
 
-// #define PRIVATE
+#define PRIVATE
 #ifdef PRIVATE
 class Derived1 : private Base {
  public:
   void showBaseData() {
-    std::cout << "showBaseData" << std::endl;
-    // std::cout << num1 << std::endl; // error: 'num1' is a private member
-    // of 'Base'
+    LOG();
+    // std::cout << num1 << std::endl;  // error: 'num1' is a private member of
+    // 'Base'
     std::cout << num2 << std::endl;
     std::cout << num3 << std::endl;
   }
 };
 #endif
 
-// #define PROTECT
+#define PROTECT
 #ifdef PROTECT
 class Derived2 : protected Base {
  public:
   void showBaseData() {
-    std::cout << "showBaseData" << std::endl;
-    // std::cout << num1 << std::endl; // error: 'num1' is a private member
+    LOG();
+    // std::cout << num1 << std::endl;  // error: 'num1' is a private member
     // of 'Base'
     std::cout << num2 << std::endl;
     std::cout << num3 << std::endl;
@@ -50,7 +70,7 @@ class Derived2 : protected Base {
 class Derived3 : public Base {
  public:
   void showBaseData() {
-    std::cout << "showBaseData" << std::endl;
+    LOG();
     // std::cout << num1 << std::endl; // error: 'num1' is a private member
     // of 'Base'
     std::cout << num2 << std::endl;
@@ -60,31 +80,28 @@ class Derived3 : public Base {
 #endif
 
 int main(void) {
-  Base b;
-  b.showData();
-
 #ifdef PRIVATE
   Derived1 d1;
   d1.showBaseData();
-  std::cout << d1.num1 << std::endl;
-  std::cout << d1.num2 << std::endl;
-  std::cout << d1.num3 << std::endl;
+  // std::cout << d1.num1 << std::endl;
+  // std::cout << d1.num2 << std::endl;
+  // std::cout << d1.num3 << std::endl;
 #endif
 
 #ifdef PROTECT
   Derived2 d2;
   d2.showBaseData();
-  std::cout << d2.num1 << std::endl;
-  std::cout << d2.num2 << std::endl;
-  std::cout << d2.num3 << std::endl;
+  // std::cout << d2.num1 << std::endl;
+  // std::cout << d2.num2 << std::endl;
+  // std::cout << d2.num3 << std::endl;
 #endif
 
 #ifdef PUBLIC
   Derived3 d3;
   d3.showBaseData();
-  std::cout << d3.num1 << std::endl;
-  std::cout << d3.num2 << std::endl;
-  std::cout << d3.num3 << std::endl;
+  // std::cout << d3.num1 << std::endl;
+  // std::cout << d3.num2 << std::endl;
+  // std::cout << d3.num3 << std::endl;
 #endif
 
   return 0;
