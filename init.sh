8ae22bb8336b4abf3c815ac0f5a91f436373c9cf
Xianglan Piao <lanxlpiao@gmail.com>
2023-05-26 13:48:00 +0900

    [feat] add init

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## global variables
+PROJECT_DIR=$(realpath $(dirname ${0}))
+echo ${PROJECT_DIR}
+
+## ansi colors
+REDBOLD="\033[1;31m"
+GREENBOLD="\033[1;32m"
+PURPLEBOLD="\033[1;34m"
+WHITEBOLD="\033[1;37m"
+RESET="\033[0m"
+
+## functions
+function check() {
+  ERROR=${?}
+  if [[ ${ERROR} == 0 ]]; then
+    printf "${GREENBOLD}Success !!!${RESET}\n\n"
+  else
+    printf "${REDBOLD}Failed With Error Code ${ERROR} !!!${RESET}\n\n"
+    exit ${ERROR}
+  fi
+}
+
+## main
+FLAGS='--std=c++17'
+DEBUG='-Wall -O0 -g'
+CC=clang++
+if [[ "${1}" == "docker" ]]; then
+  docker run --gpus all --privileged --rm -it -v ${PROJECT_DIR}:/work pytorch/pytorch:1.8.0-cuda11.1-cudnn8-devel
+elif [[ "${1}" == "build" ]]; then
+  $CC -o bin.${1} ${1} ${FLAGS} ${DEBUG}
+elif [[ "${1}" == "clean" ]]; then
+  rm -rf bin.*
+else
+  if [ ! -f "${1}" ]; then
+    echo "The file \"${1}\" does not exist!"
+    echo "Execute: ./init.sh FileName"
+  else
+    rm -rf bin.*
+    $CC -o bin.${1} ${1} ${FLAGS} ${DEBUG}; check
+    ./bin.${1}; check; rm -rf bin.*
+  fi
+fi
